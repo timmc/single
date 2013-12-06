2e166e329b523ade96d222f8b743aa5f8758ee21
Tim McCormack <tmccormack@brightcove.com>
2013-12-06 13:29:08 -0500

    correct patrolLoop

--- sequences.js
+++ sequences.js
@@ -1,9 +1,12 @@
-// Attempt a patrol loop (forward, turn around, forward, turn around)
-doSequence([
-  dtw(-100, 0, 500), dtw(-100, 0, 500), dtw(-100, 0, 500), dtw(-100, 0, 500),
-  dtw(0, 0, 750),
-  tw(180, 300),
-  dtw(-100, 0, 500), dtw(-100, 0, 500), dtw(-100, 0, 500), dtw(-100, 0, 500),
-  dtw(0, 0, 750),
-  tw(180, 300)
-]);
+// Turn around. (Don't use 180; it (weirdly) alternates left vs. right.)
+var sAboutFace = [tw(90, 1500), tw(90, 1500)];
+// Repeat this for smooth forward motion.
+var cForward = dtw(-100, 0, 1500);
+// Use this component after the last cForward (allow balancing)
+var cStabilize = dtw(0,0,1000);
+var sHalfPatrol = [cForward, cForward, cForward, cStabilize].concat(sAboutFace);
+// A patrol loop (forward, turn around, forward, turn around)
+var patrolLoop = sHalfPatrol.slice().concat(sHalfPatrol);
+
+// Run it:
+doSequence(patrolLoop);
