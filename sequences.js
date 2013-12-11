8ed23cd6dde76aa19e724b5c9cde7fb57f51fba1
Tim McCormack <tmccormack@brightcove.com>
2013-12-11 15:45:06 -0500

    Bump forward-run stabilization timeout by 30%.

--- sequences.js
+++ sequences.js
@@ -7,7 +7,7 @@ var aboutFace = [tw(90, 1500), tw(90, 1500)];
 // Repeat this for smooth forward motion.
 var cForward = dtw(-100, 0, 1500);
 // Use this command after the last cForward (allow balancing)
-var cStabilize = dtw(0,0,1000);
+var cStabilize = dtw(0,0,1300);
 
 /*
  * Sample sequences
