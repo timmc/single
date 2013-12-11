d9e093a1668ee206067e5a2a489d39d26f5f58a3
Tim McCormack <tmccormack@brightcove.com>
2013-12-11 15:44:28 -0500

    Drop s-prefix on sequences.

--- sequences.js
+++ sequences.js
@@ -1,12 +1,22 @@
+/*
+ * Basic commands and sequences -- building blocks for other sequences.
+ */
+
 // Turn around. (Don't use 180; it (weirdly) alternates left vs. right.)
-var sAboutFace = [tw(90, 1500), tw(90, 1500)];
+var aboutFace = [tw(90, 1500), tw(90, 1500)];
 // Repeat this for smooth forward motion.
 var cForward = dtw(-100, 0, 1500);
-// Use this component after the last cForward (allow balancing)
+// Use this command after the last cForward (allow balancing)
 var cStabilize = dtw(0,0,1000);
-var sHalfPatrol = [cForward, cForward, cForward, cStabilize].concat(sAboutFace);
-// A patrol loop (forward, turn around, forward, turn around)
-var patrolLoop = sHalfPatrol.slice().concat(sHalfPatrol);
 
-// Run it:
+/*
+ * Sample sequences
+ */
+
+var halfPatrol = [cForward, cForward, cForward, cStabilize].concat(aboutFace);
+// A patrol loop (forward, turn around, forward, turn around)
+var patrolLoop = [].concat(halfPatrol, halfPatrol);
+
+
+// Demo:
 doSequence(patrolLoop);
