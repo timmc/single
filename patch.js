21d4955bbd2d48b524f957e3b6a0ff3b0b208763
Tim McCormack <tmccormack@brightcove.com>
2013-12-12 15:02:42 -0500

    Timed and distance-estimation driving.

--- patch.js
+++ patch.js
@@ -1,3 +1,7 @@
+/*
+ * Movement primitives
+ */
+
 function dtw(d, t, w) {
     return {type: "d", drive: d, turn: t, wait: w};
 }
@@ -5,6 +9,10 @@ function dtw(d, t, w) {
 function tw(t, w) {
     return {type: "t", turn: t, wait: w};
 }
+
+/*
+ * Automation
+ */
 
 var noAutomation = false;
 
@@ -36,4 +44,38 @@ function doSequence(seq, k) {
         }
     }
     doNext();
+}
+
+/*
+ * Sequence builders
+ */
+
+function repeatSeq(seq, n) {
+    var ret = [];
+    for (var i = 0; i < n; i++) {
+        ret = ret.concat(seq);
+    }
+    return ret;
+}
+
+// Drive for a total of total_w millis in chunks of at most chunk_w.
+function driveTimed(d, t, total_w, chunk_w) {
+    var ret = [], remain = total_w;
+    while (remain > 0) {
+        var w = Math.min(remain, chunk_w);
+        ret.push(dtw(d, t, w));
+        remain -= w;
+    }
+    return ret;
+}
+
+// Distance in feet, ratio to max speed
+function driveDist(distance, ratio) {
+    // Compute how many milliseconds we would have to travel at "light speed"
+    // (maximum forward speed) to cover this distance.
+    var lightMilliSeconds = distance * 400;
+    var totalTime = Math.abs(lightMilliSeconds / ratio);
+    var chunkTime = 1500 * ratio;
+    var d = -100 * ratio;
+    return driveTimed(d, 0, totalTime, chunkTime);
 }
