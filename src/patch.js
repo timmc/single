eb1f74dc110689141410fbf288653bb49703c8cf
Tim McCormack <tmccormack@brightcove.com>
2014-01-24 13:59:28 -0500

    Document primitives and add distance/velocity conversions

--- src/patch.js
+++ src/patch.js
@@ -2,12 +2,52 @@
  * Movement primitives
  */
 
+// "drive" is -100 for full speed ahead, -200 is full speed backwards.
+// Don't blame me, that's just the command language.
+
+// I'm not entirely sure what "turn" does while driving. It may be
+// more about the ratio in rotation rate between the wheels, or about
+// the turning radius.
+
+// In turn-only mode, "turn" behaves normally between -178 and 178
+// (degrees CW and CCW respectively), but for values +/- 179 and 180
+// will alternate direction. I have no idea why. Larger values seem to
+// be ignored, although they used to just be cut off at 270-ish.
+
+// "wait" is time in milliseconds before executing next command. It is
+// not part of the Double's command language.
+
+/**
+ * Drive while turning. Effective max turn at drive=0 is ~75
+ * degrees.
+ */
 function dtw(d, t, w) {
     return {type: "d", drive: d, turn: t, wait: w};
 }
 
+/**
+ * Turn in place (can turn faster/farther than a dtw command.)
+ */
 function tw(t, w) {
     return {type: "t", turn: t, wait: w};
+}
+
+/*
+ * Movement abstractions
+ */
+
+// The notion of "velocity" is an abstraction on top of the Double
+// primitives. It ranges from 1 (full speed ahead) to -1 (full speed
+// backwards).
+
+/** Convert velocity to drive. */
+function vToD(v) {
+    return v * (v > 0 ? -100 : -200);
+}
+
+/** Convert drive to velocity. */
+function dToV(d) {
+    return d / (d > 0 ? -100 : -200);
 }
 
 /*
